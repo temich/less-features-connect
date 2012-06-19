479968b83b9f4136de29cb55f547c7ff329151f4
tema <tema.gurtovoy@gmail.com>
2012-06-19 16:26:07 +0400

    Configurable base path

--- index.js
+++ index.js
@@ -1,49 +1,49 @@
 var less = require('less-context-functions'),
-    path = require('path'),
-    url = require('url'),
-    qs = require('querystring'),
-    fs = require('fs');
+	path = require('path'),
+	url = require('url'),
+	qs = require('querystring'),
+	fs = require('fs');
 
-var root = path.resolve(process.cwd(), '..'),
+var root = '',
 	config = { libs: [] },
 	cache = {},
 	logger;
 
 function filename(uri) {
-    return path.resolve(root, url.parse(uri).pathname.substr(1)).replace(/\.css\b/, '.less');
+	return path.resolve(root, url.parse(uri).pathname.substr(1)).replace(/\.css\b/, '.less');
 }
 
 function feature(feats) {
-    return function(n) {
-        return feats.indexOf(n.value) !== -1 ? less.tree.True : less.tree.False;
-    }
+	return function(n) {
+		return feats.indexOf(n.value) !== -1 ? less.tree.True : less.tree.False;
+	}
 }
 
 function features(uri) {
-    var s = url.parse(uri).search,
-        q = s ? qs.parse(s.substring(1)) : undefined,
-        feats = [];
+	var s = url.parse(uri).search,
+		q = s ? qs.parse(s.substring(1)) : undefined,
+		feats = [];
 
-    if (q && q.features)
-        feats = q.features.split('|');
+	if (q && q.features)
+		feats = q.features.split('|');
 
-    return feats;
+	return feats;
 }
 
 function options(filename, url) {
-    var opts = {};
+	var opts = {};
 
-    opts.paths = [path.dirname(filename), config.libs];
-    opts.compress = config.compress;
-    opts.functions = { feature: feature(features(url)) };
+	opts.paths = [path.dirname(filename), config.libs];
+	opts.compress = config.compress;
+	opts.functions = { feature: feature(features(url)) };
 
-    return opts;
+	return opts;
 }
 
 function read(file, next) {
 	fs.readFile(file, 'utf-8', function (err, data) {
 		logger && logger.trace('Reading file ' + file);
-		
+
 		next(err, data);
 	});
 }
@@ -52,24 +52,24 @@ function handle(req, res, next) {
 	if (cache[req.url])
 		return res.css(cache[req.url]);
 
-    logger && logger.trace('Processing request ' + req.url);
+	logger && logger.trace('Processing request ' + req.url);
 
-    var file = filename(req.url);
+	var file = filename(req.url);
 
 	read(file, function (err, data) {
 		if (err) {
 			if (err.code !== 'ENOENT') {
-				res.error().end();
+				res.error().send();
 				throw err;
 			} else
-				return res.notFound().end();
+				return res.notFound().send();
 		}
 
 		logger && logger.trace('Rendering file ' + file);
 
 		less.render(data, options(file, req.url), function (err, data) {
 			if (err) {
-				res.error().end();
+				res.error().send();
 				throw err;
 			}
 
@@ -78,7 +78,7 @@ function handle(req, res, next) {
 			config.caching && (cache[req.url] = data);
 			res.css(data);
 
-            logger && logger.trace('%s has been served successfully', req.url);
+			logger && logger.trace('%s has been served successfully', req.url);
 		});
 	});
 }
@@ -86,6 +86,7 @@ function handle(req, res, next) {
 module.exports = function (cfg, log) {
 	config = cfg;
 	logger = log;
-	
+	root = path.resolve(process.cwd(), cfg.base);
+
 	return handle;
 };
